4653bb5e8b90f2f7f5c73319eecad48449327b2d
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2022-03-18 23:05:35 +0530

    add to another file changes and POST api

--- app/src/main/java/com/edulifesolutions/retrofitpostapi/MainActivity.java
+++ app/src/main/java/com/edulifesolutions/retrofitpostapi/MainActivity.java
@@ -3,30 +3,59 @@ package com.edulifesolutions.retrofitpostapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button Send_Post_Request_Button;
+    private Button btnSendPostRequest;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Send_Post_Request_Button = findViewById(R.id.Send_Post_Request_Button);
+        btnSendPostRequest = findViewById(R.id.btnSendPostRequest);
 
-        Send_Post_Request_Button.setOnClickListener(new View.OnClickListener() {
+        btnSendPostRequest.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
+                btnSendPostRequestClicked();
             }
         });
     }
 
-    private void Send_Post_Request_Button() {
+    private void btnSendPostRequestClicked() {
+
+        ApiInterface apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+        Call<User> call = apiInterface.getUserInformation("Kasun Weerasekara", "Software Engineer-Mobile Developer");
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+                Log.e(TAG, "onResponse: " +response.code());
+                Log.e(TAG, "onResponse: name :" +response.body().getName());
+                Log.e(TAG, "onResponse: createAt :" +response.body().getCreatedAt());
+                Log.e(TAG, "onResponse: job :" +response.body().getJob());
+                Log.e(TAG, "onResponse: id :" +response.body().getId());
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+                Log.e(TAG, "onFailure: "+t.getMessage());
+            }
+        });
 
     }
 }
